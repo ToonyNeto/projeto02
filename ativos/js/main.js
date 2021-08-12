5ec10e1614f8fa943c048c0981d913c1d219b8fd
unknown <toony.neto'gmail.com>
2021-08-12 17:45:14 -0300

    atualizando tabela de pedidos

--- ativos/js/main.js
+++ ativos/js/main.js
@@ -21,5 +21,5 @@ $(document).ready(function(){
 // =============order show data table===========
 
 $(document).ready( function () {
-    $('#example').DataTable();
+    $('#order-table').DataTable();
 } );
